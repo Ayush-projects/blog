6031976ebb67839dd16c852419d136bbbe6a1bbe
Ayush <kayush479@gmail.com>
2022-04-06 19:07:15 +0530

    Ayush

--- app.js
+++ app.js
@@ -3,6 +3,26 @@ const app = express();
 const mongoose = require('mongoose')
 
 
+let db_url = "mongodb://admin:admin@cluster0-shard-00-00.ngnbw.mongodb.net:27017,cluster0-shard-00-01.ngnbw.mongodb.net:27017,cluster0-shard-00-02.ngnbw.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-y66jbw-shard-0&authSource=admin&retryWrites=true&w=majority"
+
+
+
+mongoose.connect(db_url).then((obj)=>{ console.log("Connecetd to database")}).catch((err)=>{
+    console.log(err)
+})
+
+
+
+
+app.set("view engine", "ejs");
+
+
+
+
+
+app.get("/", (req,res)=>{
+    res.render("index.ejs");
+})
 
 
 
---
+++ schema/blog.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose")
+
+
+
+const blogSchema = new mongoose.Schema({
+
+
+    blog_main_title: {
+        type: String,
+        required: True
+    },
+
+
+    blog_secondary_title:{
+        type: String,
+        required: True
+    },
+
+
+
+
+
+
+
+
+
+
+})
+
+
